4525809afb086947dc75a2bcef9009926df3268e
natecross123 <137192717+natecross123@users.noreply.github.com>
2024-10-23 20:41:51 -0500

    Update tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,28 @@
 document.addEventListener('DOMContentLooaded', () =>{
     const squares = document.querySelectorAll('.game-board div');
-    squares.forEach((square)=>{
-        square.classList.add('square');
-    })
+    const statusDisplay = document.getElementById('status');
+    let currentplayer = 'x';
+    let gamestate = Array(9).fill(null);
 
-})
+    squares.forEach((square,index)=>{
+        square.classList.add('square');
+        
+    square.addEventListener('click',function(){
+        handleclick(square,index);
+    });
+
+});
+function handleclick(element,index){
+    if(gamestat[index] || checkWinner()) return;
+
+    gamestate[index] = currentplayer;
+    element.textcontent = currentplayer;
+    element.classList.add(currentplayer);
+
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    statusDisplay.textContent = `It's ${currentPlayer}'s turn`;
+
+    }
+
+
+});
